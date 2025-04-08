bf1baa99543b8564baaeb337fadbf9d011827423
pyyupsk <pyyupsk@proton.me>
2025-04-08 14:13:05 +0700

    chore: update ESLint configuration and dependencies for frontend app

---
+++ apps/frontend/eslint.config.mjs
@@ -0,0 +1,25 @@
+import base from "@packages/eslint-config/base";
+import prettier from "@packages/eslint-config/prettier";
+import vue from "eslint-plugin-vue";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+const eslintConfig = tseslint.config(
+  base,
+  {
+    extends: [...vue.configs["flat/recommended"]],
+    files: ["**/*.{ts,vue}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        definePage: "readonly",
+      },
+    },
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
+  },
+  prettier,
+);
+
+export default eslintConfig;
---
+++ packages/eslint-config/base.mjs
@@ -0,0 +1,63 @@
+import eslint from "@eslint/js";
+import perfectionist from "eslint-plugin-perfectionist";
+import turbo from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+import onlywarn from "eslint-plugin-only-warn";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  perfectionist.configs["recommended-natural"],
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+      sourceType: "module",
+    },
+  },
+  {
+    plugins: {
+      turbo,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+  {
+    plugins: {
+      onlywarn,
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          fixStyle: "separate-type-imports",
+          prefer: "type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  {
+    ignores: ["dist/**"],
+  },
+);
